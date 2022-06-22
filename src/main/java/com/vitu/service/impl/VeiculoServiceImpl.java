2c3e576976aaeaec4b4a310f0c300755d4b47b53
Vítor Norberto <vithor.norberto@gmail.com>
2022-06-21 23:23:02 -0300

    Adiciona métricas com prometheus

--- src/main/java/com/vitu/service/impl/VeiculoServiceImpl.java
+++ src/main/java/com/vitu/service/impl/VeiculoServiceImpl.java
@@ -3,27 +3,38 @@ package com.vitu.service.impl;
 import com.vitu.domain.Veiculo;
 import com.vitu.repository.VeiculoRepository;
 import com.vitu.service.VeiculoService;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Singleton
 public class VeiculoServiceImpl implements VeiculoService {
 
-    private final VeiculoRepository veiculoRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(VeiculoServiceImpl.class);
 
-    public VeiculoServiceImpl(VeiculoRepository veiculoRepository) {
+    private final VeiculoRepository veiculoRepository;
+    private final MeterRegistry meterRegistry;
+
+    public VeiculoServiceImpl(VeiculoRepository veiculoRepository, MeterRegistry meterRegistry) {
         this.veiculoRepository = veiculoRepository;
+        this.meterRegistry = meterRegistry;
     }
 
     @Override
     public Veiculo criar(Veiculo veiculo) {
+        meterRegistry.counter("veiculo.create").increment();
         return veiculoRepository.save(veiculo);
     }
 
     @Override
     public Veiculo buscarPorId(Long id) {
-        return veiculoRepository.findById(id).orElseThrow();
+        LOG.info("Buscando veículo pelo id: {}", id);
+        Veiculo veiculo = veiculoRepository.findById(id).orElseThrow();
+        LOG.info("Veículo encontrado: {}", veiculo);
+        return veiculo;
     }
 
     @Override
